2298a2f2dd0c58b54519065c67e4bcca85356f1f
rajshetye11 <rajshetye11@gmail.com>
2021-08-28 11:52:31 +0530

    [raj] : added Player and Computer Letter

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,17 +1,40 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	char [] board = new char[10];
+	Scanner sc = new Scanner(System.in);
 	
 	public void ticTacToeBoard() {
+		char [] board = new char[10];
 		for(int i = 1; i<board.length ; i++) {
-			board[i] = 0;
+			board[i] = ' ';
+		}
+	}
+	
+	public void selectXorO() {
+		System.out.println("Select X or O");
+		String xoro = sc.nextLine();
+		char player, comp;
+		if(xoro.equals("X")) {
+			System.out.println("Player selected X");
+			System.out.println("Computer Letter O");
+			player = 'X';
+			comp ='O';
+		}else if(xoro.equals("O")) {
+			System.out.println("Player selected O");
+			System.out.println("Computer Letter X");
+			player = 'O';
+			comp ='X';
+		}else {
+			System.out.println("Enter Valid Letter");
 		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome To TicTacToe ");
 		TicTacToeGame t1 = new TicTacToeGame();
+		t1.selectXorO();
 		t1.ticTacToeBoard();
 	}
 }
